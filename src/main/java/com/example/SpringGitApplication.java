4bd90be47c851418676ab8c98ec8bd73ec5cec44
jyoti <jyoti.pandey@credmudra.com>
2023-09-08 16:14:46 +0530

    added a Readme file

--- src/main/java/com/example/SpringGitApplication.java
+++ src/main/java/com/example/SpringGitApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringGitApplication {
+public class SpringGitApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+
 		SpringApplication.run(SpringGitApplication.class, args);
+		System.out.println("Learning Git Hub with Intellij Idea");
+
 	}
 
 }
